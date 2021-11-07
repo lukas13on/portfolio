6084d98e46ffffca57f60694b403f04819e7d19e
lukas13on <luka.pc.pc@gmail.com>
2021-11-06 23:25:36 -0300

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,5 +10,8 @@ export default {
         subFolders: true
     },
     target: 'static', // 'static' or 'server'
+    router: {
+        base: '/portfolio/' // remove router for server
+    },
     ssr: true // true for static, false for server
 };
