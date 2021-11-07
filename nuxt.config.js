29ce71b0d06b540468fc6f495617b2f6d985ad6c
lukas13on <luka.pc.pc@gmail.com>
2021-11-07 18:05:25 -0300

    aa

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,5 @@ export default {
     generate: {
         dir: 'dist'
     },
-    target: 'static', // 'static' or 'server'
-    router: {
-        base: '/portfolio/' // remove router for server
-    },
-    ssr: false // true for static, false for server
+    target: 'static'
 };
